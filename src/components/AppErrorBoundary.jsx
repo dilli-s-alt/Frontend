7abe917ca6f97d3a477f8fb2107c90dabdd340cd
dilli-s-alt <lathakiranlatha740@gmail.com>
2026-04-01 22:10:18 +0530

    Initial commit

--- src/components/AppErrorBoundary.jsx
+++ src/components/AppErrorBoundary.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import { Component } from "react";
 
-export default class AppErrorBoundary extends React.Component {
+export default class AppErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false, message: "" };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AppErrorBoundary from "./components/AppErrorBoundary.jsx";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <AppErrorBoundary>
       <App />
     </AppErrorBoundary>
-  </React.StrictMode>
+  </StrictMode>
 );
